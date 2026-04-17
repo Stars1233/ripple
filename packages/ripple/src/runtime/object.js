93220b021aa2ab197ebbccbca30169210b480dca
Dominic Gannaway <dg@domgan.com>
2026-04-17 21:36:57 +0100

    missing parts

--- packages/ripple/src/runtime/object.js
+++ packages/ripple/src/runtime/object.js
@@ -1,29 +1,25 @@
 /** @import { Block } from '#client' */
-/** @import { RippleObject as RippleObjectT } from '#public' */
 import { safe_scope } from './internal/client/runtime.js';
 import { object_proxy } from './proxy.js';
 
 /**
  * @template {object} T
- * @constructor
- * @param {T} obj
- * @returns {RippleObjectT<T>}
  */
-export function RippleObject(obj) {
-	if (!new.target) {
-		throw new Error("RippleObject must be called with 'new'");
+export class RippleObject {
+	/**
+	 * @param {T} obj
+	 */
+	constructor(obj) {
+		var block = safe_scope();
+		return /** @type {RippleObject<any>} */ (/** @type {unknown} */ (ripple_object(block, obj)));
 	}
-
-	var block = safe_scope();
-
-	return ripple_object(block, obj);
 }
 
 /**
  * @template {object} T
  * @param {T} obj
  * @param {Block} block
- * @returns {RippleObjectT<T>}
+ * @returns {RippleObject<T>}
  */
 export function ripple_object(block, obj) {
 	return object_proxy(obj, block);
